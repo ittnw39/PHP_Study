b5aab65d81ab6f11e2bd1524740a6620e8f243b1
ittnw39 <ittnw39@gmail.com>
2025-06-27 10:08:00 +0900

    250627

---
+++ Part1/ch1/updatting-variables.php
@@ -0,0 +1,19 @@
+<?php
+$name = 'Ivy';
+$name = 'James';
+$price = 10.99;
+$isStudent = true;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Welcome to the Candy Store</h1>
+    <p>Name: <?php echo $name; ?></p>
+</body>
+</html>
---
+++ Part1/ch1/variables.php
@@ -0,0 +1,22 @@
+<?php
+$name = 'Ivy';
+$age = 20;
+$price = 10.99;
+$isStudent = true;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Welcome to the Candy Store</h1>
+    <p>Name: <?php echo $name; ?></p>
+    <p>Age: <?php echo $age; ?></p>
+    <p>Price: <?php echo $price; ?></p>
+    <p>Is Student: <?php echo $isStudent; ?></p>
+</body>
+</html>
